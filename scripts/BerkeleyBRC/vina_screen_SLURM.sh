32a89d6bed1ae5a34da2429fa996d54e339e027d
Andrew-C-Lu <andrew.lu.chun@gmail.com>
2017-04-15 12:46:44 -0700

    Add files via upload

---
+++ scripts/BerkeleyBRC/vina_screen_SLURM.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+for f in ligand_*.pdbqt; do
+    b=`basename $f .pdbqt`
+    echo Processing ligand $b
+    j=${b}.job
+    d=`pwd`
+    mkdir -p $b
+    echo "#! /bin/bash" > $j
+    echo "cd $d" > $j
+    echo "vina --config conf.txt --cpu 1 --ligand $f --out ${b}/out.pdbqt --log ${b}/log.txt &gt ${b}/stdout 2&gt ${b}/stderr" >> $j
+    chmod +x $j
+    #qsub -l cput=00:30:00 -l nodes=1:ppn=1 -l walltime=00:30:00 -l mem=512mb $j
+    sbatch -t[30] --mem=512mb $j
+done
